2bc448efb7c289b299330894bcbbe508c70120e4
Айдар <Aidar@niimm-304c.i.ksu.ru>
2014-10-23 18:30:46 +0400

    123

---
+++ Afsdfs/Afsdfs/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Afsdfs
+//
+//  Created by Айдар on 23.10.14.
+//  Copyright (c) 2014 Айдар. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Afsdfs/Afsdfs/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Afsdfs
+//
+//  Created by Айдар on 23.10.14.
+//  Copyright (c) 2014 Айдар. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
